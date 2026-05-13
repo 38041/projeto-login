24c91c6c681c7557791f2d0d3a224277095fe37c
38041 <alessarovigatti.talita@gmail.com>
2026-05-12 23:55:42 -0300

    Add files via upload

--- Usuarios.jsx
+++ Usuarios.jsx
@@ -3,30 +3,8 @@ import './Usuarios.css'
 function Usuarios() {
   return (
     <div className="usuarios">
-      <h1>Usuarios de Cadastro</h1>
-
-          <form>
-            <div className="input-group">
-              <label>Nome</label>
-              <input type="text" placeholder="Digite seu Nome..." />
-            </div>
-
-           <div className="input-group">
-              <label>Email</label>
-              <input type="text" placeholder="Digite seu Email..." />
-            </div>
-
-            <div className="input-group">
-              <label>Nova Senha</label>
-              <input type="password" placeholder="Digite sua Nova senha..." />
-            </div>
-
-            <button type="submit">Cadastrar</button>
-
-  
-          </form>
-        </div>
-      
+      <h1>Usuarios</h1>
+    </div>
   )
 }
 
